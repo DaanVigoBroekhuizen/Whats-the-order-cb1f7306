0c929d24ebb2bdfd69ffc64ebc60f90647f70e51
daan <2087884@talnet.nl>
2020-03-11 09:52:53 +0100

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -28,3 +28,7 @@ SELECT * FROM series WHERE seasons > 5;
 SELECT * FROM series WHERE seasons <= 3 OR seasons >= 20;
 SELECT * FROM series WHERE title LIKE th;
 SELECT * FROM series WHERE seasons <> 3;
+
+SELECT * FROM series WHERE rating >= 2.5 ORDER BY rating DESC;
+SELECT * FROM series WHERE seasons <=5 ORDER BY seasons ASC;
+SELECT * FROM series WHERE seasons <= 3 OR seasons >= 20 ORDER BY seasons * country DESC;
